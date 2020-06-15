08998dbca3f4320894692bf794a357b40658fd75
Jeff Bai <j.bai@wustl.edu>
2020-06-15 06:32:46 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,6 +1,6 @@
 module Concerns::Findable
   def find_by_name(name)
-    self.all.detect {|char| char.name == name}
+    self.all.detect {|x| x.name == name}
   end
 
   def find_or_create_by_name(name)

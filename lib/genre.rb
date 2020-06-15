a6fbc24b4d516f02f9656850f79099d614f1e472
Jeff Bai <j.bai@wustl.edu>
2020-06-15 06:30:23 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -32,7 +32,7 @@ class Genre
   def artists
     @artists_array = []
     @songs.each do |song|
-      @artist_array.include?(song.artist) ? nil : @artists_array << song.artist
+      @artists_array.include?(song.artist) ? nil : @artists_array << song.artist
     end
     @artists_array
   end

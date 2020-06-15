7360a7bf2fd1cfc1200436e11260f52b6b33eadc
Jeff Bai <j.bai@wustl.edu>
2020-06-15 06:38:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
     self.find_by_name(name) || self.create(name)
   end
 
-  def self.new_from_filename(filename)
+  def self.create_from_filename(filename)
     self.new_from_filename(filename).save
   end
 

84fa9bf9880c413c8c6b22028ae4f7b905e761bd
Jeff Bai <j.bai@wustl.edu>
2020-06-15 06:30:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,5 +41,10 @@ class Song
     genre.songs << self unless genre.songs.include?(self)
   end
 
+  def self.find_by_name(name)
+    @@all.detect do |song|
+      song.name == name
+    end
+
 
 end

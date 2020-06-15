6e2c044e595d4de69c05e99c14b877a3062d1252
Jeff Bai <j.bai@wustl.edu>
2020-06-15 06:13:44 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+class Artist
+  attr_accessor :name, :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    artist.save
+    artist
+  end
+
+  def add_song(song)
+    song.artist == self ? song.artist : song.artist == self
+    songs.include?(song) ? songs : songs << song 
+  end
+end

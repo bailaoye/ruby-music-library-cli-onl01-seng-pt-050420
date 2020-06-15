3512ad46ea2644c90b677f404c664655c14e05b8
Jeff Bai <j.bai@wustl.edu>
2020-06-15 06:35:25 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -1,11 +1,11 @@
 class MusicImporter
   attr_accessor :filepath, :song, :artist, :genre
   def initialize(filepath)
-    @path = path
+    @filepath = filepath
   end
 
   def files
-    Dir.entries(@path).select! {|entry| entry.end_with?(".mp3")}
+    Dir.entries(@filepath).select! {|entry| entry.end_with?(".mp3")}
   end
 
 end
